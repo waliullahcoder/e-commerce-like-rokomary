4a8bcab93fcbb799e243445b9b815ebce6b6c89e
technoparkbdit <wali@technoparkbd.com>
2026-03-04 12:17:35 +0600

    added sales from order

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Coa;
+use App\Models\User;
+use App\Models\Store;
 use App\Models\Client;
 use App\Models\Sales;
 use App\Models\SalesList;
+use App\Models\Product;
+use App\Models\SalesOfficer;
+use App\Models\ProductEdition;
 use App\Models\Collection;
 use App\Models\CollectionList;
 use App\Models\AccountTransactionAuto;
 use App\Models\ProductVariant;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Services\FrontEndService;
+use Carbon\Carbon;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class AdminOrderController extends Controller
@@ -54,102 +62,126 @@ class AdminOrderController extends Controller
         $request->validate([
             'status' => 'required|string'
         ]);
-        dd($request->all(),$order,$order->items);
 
-         try {
-            DB::transaction(function () use ($request) {
-                $data = Sales::create([
-                    'client_id' => $request->client_id,
-                    'store_id'  => $request->store_id,
-                    'sales_officer_id' => $request->sales_officer_id,
-                    'coa_id'    => $request->sale_type == 'Cash' ? $request->coa_id : null,
-                    'sale_type' => $request->sale_type,
-                    'invoice'   => $this->invoiceNo(),
-                    'date'      => date('Y-m-d', strtotime($request->date)),
-                    'amount'    => $request->total_amount,
-                    'discount'  => $request->discount,
-                    'net_amount' => $request->net_amount,
-                    'paid'      => $request->sale_type == 'Cash' ? $request->net_amount : 0.00,
-                    'remarks'   => $request->remarks,
-                    'created_by' => Auth::id(),
-                ]);
-
-                foreach ($request->product_edition_id as $product_edition_id) {
-                    $discount = $request->total_amount > 0 ? ($request->discount / $request->total_amount) * $request->amount[$product_edition_id] : 0.00;
-                    SalesList::create([
-                        'sales_id' => $data->id,
-                        'store_id' => $request->store_id,
-                        'client_id' => $request->client_id,
-                        'product_id' => $request->product_id[$product_edition_id],
-                        'product_edition_id' => $product_edition_id,
-                        'price' => $request->price[$product_edition_id],
-                        'commission' => $request->commission[$product_edition_id],
-                        'commission_amount' => $request->commission[$product_edition_id] * $request->qty[$product_edition_id],
-                        'rate' => $request->rate[$product_edition_id],
-                        'qty' => $request->qty[$product_edition_id],
-                        'amount' => $request->amount[$product_edition_id],
-                        'discount' => $discount,
-                        'net_amount' => $request->amount[$product_edition_id] - $discount,
-                    ]);
-                }
-
-                $client = Client::find($request->client_id);
-                if ($client->coa) {
-                    $income_head = Coa::where('head_type', 'I')->where('head_name', 'Product Sales')->first();
-                    $headCode = collect([
-                        '0' => $client->coa->head_code,
-                        '1' => $income_head->head_code,
-                    ]);
-
-                    $debit_amount = collect([
-                        '0' => $request->net_amount,
-                        '1' => 0.00
-                    ]);
-
-                    $credit_amount = collect([
-                        '0' => 0.00,
-                        '1' => $request->net_amount,
-                    ]);
-
-                    $countHead = count($headCode);
-                    $postData = [];
-                    for ($i = 0; $i < $countHead; $i++) {
-                        $coa = Coa::where('head_code', $headCode[$i])->first();
-                        $postData[] = [
-                            'voucher_no' => $data->invoice,
-                            'voucher_type' => "Client Sales",
-                            'date' => date('Y-m-d', strtotime($request->date)),
-                            'coa_id' => $coa->id,
-                            'coa_head_code' => $headCode[$i],
-                            'narration' => 'Client Sales Against Invoice No - ' . $data->invoice,
-                            'debit_amount' => $debit_amount[$i],
-                            'credit_amount' => $credit_amount[$i],
-                            'created_by' => Auth::id(),
-                            'created_at' => Carbon::now(),
-                            'updated_at' => Carbon::now()
-                        ];
-                    }
-                    AccountTransactionAuto::insert($postData);
-                }
-
-            });
-        } catch (\Exception $e) {
-            return back()->withErrors($e->getMessage());
+        if ($request->status == 'pending') {
+            return redirect()->back()->withErrors('Ops! Status is already Exist!');
         }
 
-        $order->update([
-            'status' => $request->status
-        ]);
-
-
-        if ($request->status == 'cancelled') {
+         if ($request->status == 'cancelled') {
             // Restore stock
             foreach ($order->items as $item) {
                 ProductVariant::where('id', $item->product_variant_id)->orWhere('product_id', $item->product_id)->increment('stock', $item->qty);
             }
+        }else{
+                try {
+                    DB::transaction(function () use ($request,$order) {
+                        $order->update([
+                        'status' => $request->status
+                        ]);
+                        $client = Client::where('user_id', $order->user_id)->first();
+                        $store = Store::first();
+                        $sales_officer = SalesOfficer::where('email', 'online@gmail.com')->first();
+                        $data = Sales::create([
+                            'client_id' => $client->id,
+                            'store_id'  => $store ? $store->id : 1,
+                            'sales_officer_id' => $sales_officer? $sales_officer->id : 1,
+                            'coa_id'    => $client->coa->id,
+                            'sale_type' => 'Credit',
+                            'invoice'   => $this->invoiceNo(),
+                            'date'      => date('Y-m-d', strtotime($order->created_at)),
+                            'amount'    => $order->subtotal,
+                            'discount'  => $order->discount,
+                            'net_amount' => $order->total,
+                            'paid'      => 0.00,
+                            'remarks'   => 'Online Order',
+                            'created_by' => Auth::id(),
+                        ]);
+
+                        foreach ($order->items as $item) {
+                            $product= Product::find($item->product_id);
+                            $product_edition_id = ProductEdition::where('product_id', $item->product_id)->first()->id ?? 1;
+                            SalesList::create([
+                                'sales_id' => $data->id,
+                                'store_id' => $store ? $store->id : 1,
+                                'client_id' => $client->id,
+                                'product_id' => $item->product_id,
+                                'product_edition_id' => $product_edition_id,
+                                'price' => $item->price,
+                                'commission' => 0,
+                                'commission_amount' => 0,
+                                'rate' => $product->sale_price,
+                                'qty' => $item->qty,
+                                'amount' => $item->total,
+                                'discount' => $order->discount,
+                                'net_amount' => $item->total - $order->discount,
+                            ]);
+                        }
+
+                        if ($client->coa) {
+                            $income_head = Coa::where('head_type', 'I')->where('head_name', 'Product Sales')->first();
+                            $headCode = collect([
+                                '0' => $client->coa->head_code,
+                                '1' => $income_head->head_code,
+                            ]);
+
+                            $debit_amount = collect([
+                                '0' => $request->net_amount,
+                                '1' => 0.00
+                            ]);
+
+                            $credit_amount = collect([
+                                '0' => 0.00,
+                                '1' => $request->net_amount,
+                            ]);
+
+                            $countHead = count($headCode);
+                            $postData = [];
+                            for ($i = 0; $i < $countHead; $i++) {
+                                $coa = Coa::where('head_code', $headCode[$i])->first();
+                                $postData[] = [
+                                    'voucher_no' => $data->invoice,
+                                    'voucher_type' => "Client Sales",
+                                    'date' => date('Y-m-d', strtotime($request->date)),
+                                    'coa_id' => $coa->id,
+                                    'coa_head_code' => $headCode[$i],
+                                    'narration' => 'Client Sales Against Invoice No - ' . $data->invoice,
+                                    'debit_amount' => $debit_amount[$i],
+                                    'credit_amount' => $credit_amount[$i],
+                                    'created_by' => Auth::id(),
+                                    'created_at' => Carbon::now(),
+                                    'updated_at' => Carbon::now()
+                                ];
+                            }
+                            AccountTransactionAuto::insert($postData);
+                        }
+
+                    });
+                } catch (\Exception $e) {
+                    dd($e);
+                    return back()->withErrors($e->getMessage());
+                }
+
         }
+         
+         
+
+      
 
         return redirect()->back()->withSuccessMessage('Order status updated successfully ✅');
+    }
+
+     /**
+     * Generate Invoice No.
+     */
+    public function invoiceNo()
+    {
+        $data = Sales::withTrashed()->select(['invoice'])->whereDate('created_at', '>=', date('Y-m-01'))->whereDate('created_at', '<=', date('Y-m-t'))->orderBy('id', 'desc')->first();
+        if ($data) {
+            $trim = (int)str_replace("CS", '', $data->invoice) + 1;
+            return "CS" . $trim;
+        } else {
+            return "CS" . date('ym') . '001';
+        }
     }
 
 
